4b9fc5b357c60ecde412f4633b14991837280f9d
IsraelSantos7792 <israelqueren1805@gmail.com>
2021-11-16 23:39:17 -0300

    TotalProject

---
+++ config/webpack/environment.js
@@ -0,0 +1,16 @@
+const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'provide',
+
+new webpack.ProvidePlugin({
+  $: 'jquery',
+  jQuery: 'jquery',
+  jquery: 'jquery',
+  Popper: ['popper.js', 'default']
+})
+)
+
+
+module.exports = environment
